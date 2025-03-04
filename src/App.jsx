fb62cbb7faa235f9fe69a177fb942b309519b441
RBritney <britneyrangith@gmail.com>
2025-03-04 20:30:58 +0100

    Building TopBar part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import TopBar from './topbar/TopBar'
 
 function App() {
 
   return (
-    <div>DS Blog</div>
-
+    <>
+      <TopBar/>
+    </>
   )
 }
 
