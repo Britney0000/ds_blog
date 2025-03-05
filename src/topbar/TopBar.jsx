a5960e77a2825030d0c688e98a7e6bf084b7bcc4
RBritney <britneyrangith@gmail.com>
2025-03-05 21:09:52 +0100

    Building TopBar

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import './topbar.css'
+import dsImage from "../assets/ds.jpeg";
+
+
 
 export default function TopBar() {
   return (
-    <div className='top'>TopBar</div>
+    <div className='top'>
+      <div className= "topLeft">
+        <i className="topIcon fa-brands fa-github"></i>
+        <i className="topIcon fa-brands fa-pinterest"></i>
+      </div>
+
+      <div className= "topCenter">
+      <ul className='topList'>
+            <li className="topListItem">HOME</li>
+            <li className="topListItem">ABOUT</li>
+            <li className="topListItem">CONTACT</li>
+            <li className="topListItem">WRITE</li>
+            <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+
+      <div className= "topRight">
+        <img className="topImage" src={dsImage} alt="Demon Slayer"/>
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
   )
 }
